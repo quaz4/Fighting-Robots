82febe7c51fad34832417d96ee07717cb26ccf30
William Stewart <hmaswill@gmail.com>
2019-10-24 10:49:48 +0800

    End threads of window close
    - Override default close handler to end AI threads on window close

--- main_application/src/main/java/art/willstew/arena/javafx/ExampleJFXApp.java
+++ main_application/src/main/java/art/willstew/arena/javafx/ExampleJFXApp.java
@@ -1,6 +1,10 @@
 package art.willstew.arena.javafx;
 
+// import java.beans.EventHandler;
+
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
@@ -8,6 +12,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class ExampleJFXApp extends Application {
     public static void main(String[] args) {
@@ -52,6 +57,19 @@ public class ExampleJFXApp extends Application {
         Scene scene = new Scene(contentPane, 800, 800);
         stage.setScene(scene);
         stage.show();
+
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent we) {
+                Platform.exit();
+
+                try {
+                    arena.stop();
+                } catch(IllegalStateException e) {
+                    // Safe to ignore
+                }
+            }
+        });
     }
 
     public void log(String message) {
